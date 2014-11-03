9ec606690669067f51d28fbba96786f0e5705fb9
arebecchi <arebecchi@devartis.com>
2014-11-02 21:34:03 -0300

    creo que meti mas bugs de los que saque

--- source/ejemplo.py
+++ source/ejemplo.py
@@ -5,8 +5,12 @@ import estimators
 # Creo una instancia de la clase que representa al metodo
 # 'Histograma Clasico'
 aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=20)
+bEstimator = estimators.DistributionSteps('db.sqlite3', 'table1', 'c1', parameter=20)
 
 # Pruebo distintas instancias de estimacion
 print "Classic Histogram"
 print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
 print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
+print "Distribution Steps"
+print "  Sel(=%d) : %3.2f" % (50, bEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.2f" % (70, bEstimator.estimate_greater(70))
--- source/estimators.py
+++ source/estimators.py
@@ -7,6 +7,7 @@ import sqlite3
 
 TABLE_NAME = 'table1'
 
+
 class Estimator(object):
     """Clase base de los estimadores."""
 
@@ -33,14 +34,20 @@ class Estimator(object):
 
 
 class ClassicHistogram(Estimator):
-
     def build_struct(self):
-        self.eq_histogram = [0 for i in xrange(self.parameter+1)]
-        self.gt_histogram = [0 for i in xrange(self.parameter+1)]
+        self.eq_histogram = [0 for i in xrange(self.parameter + 1)]
+        self.gt_histogram = [0 for i in xrange(self.parameter + 1)]
 
         self.connect()
-    #   buscar minimo y maximo y ver de que valor a que valor va el intervalo?
-    #   o directamente sobre el rango?
+        # buscar minimo y maximo y ver de que valor a que valor va el intervalo?
+        #   o directamente sobre el rango?
+
+        c = self.conn.cursor()
+        self.total = 0
+
+        for row in c.execute('SELECT count(*) FROM %s' % (TABLE_NAME, )):
+            value = row[0]
+            self.total = value
 
         first = True
 
@@ -54,9 +61,9 @@ class ClassicHistogram(Estimator):
 
         val_range = self.max_val - self.min_val
 
-        self.bucket_width = int(val_range/self.parameter)   # cantidad de valores que representa una columna del histo
+        self.bucket_width = int(val_range / self.parameter)  # cantidad de valores que representa una columna del histo
 
-    #   recorrer la base y contar cuantos entran en cada columna
+        #   recorrer la base y contar cuantos entran en cada columna
         c = self.conn.cursor()
         for row in c.execute('SELECT %s FROM %s ORDER BY %s ASC' % (self.column, TABLE_NAME, self.column)):
             value = row[0]
@@ -67,37 +74,116 @@ class ClassicHistogram(Estimator):
 
     def estimate_equal(self, value):
         columns = self.get_columns_for(value)
-        return self.eq_histogram[columns[0]] / self.bucket_width
+        return float((self.eq_histogram[columns[0]] / self.bucket_width))
 
     def estimate_greater(self, value):
         columns = self.get_columns_for(value)
         if len(columns) == 1:
             return self.gt_histogram[columns[0]]
         else:
-            return (self.gt_histogram[columns[0]] + self.gt_histogram[columns[1]]) / 2
+            return float((self.gt_histogram[columns[0]] + self.gt_histogram[columns[1]]) / 2)
 
     def get_columns_for(self, value):
-        #si es exactamente min + bucket_width * k va a ser una sola, sino 2 y promedio los valores
+        # si es exactamente min + bucket_width * k va a ser una sola, sino 2 y promedio los valores
         if value % self.bucket_width == 0 or value == self.max_val:
-            return [int((value-self.min_val) / self.bucket_width)]
+            return [int((value - self.min_val) / self.bucket_width)]
         else:
-            return [int((value-self.min_val) / self.bucket_width), int((value-self.min_val) / self.bucket_width)+1]
+            return [int((value - self.min_val) / self.bucket_width),
+                    int((value - self.min_val) / self.bucket_width) + 1]
 
 
 class DistributionSteps(Estimator):
-
     def build_struct(self):
-        pass
+        self.histogram = [0 for i in xrange(self.parameter + 1)]
+
+        self.connect()
+
+        c = self.conn.cursor()
+        self.total = 0
+
+        for row in c.execute('SELECT count(*) FROM %s' % (TABLE_NAME, )):
+            value = row[0]
+            self.total = value
+
+        items_per_bucket = int(self.total / self.parameter)
+
+        #   recorrer la base y meter items_per_bucket en cada columna
+        c = self.conn.cursor()
+        items_in_current_bucket = 0
+        current_bucket = 0
+        value = 0   #fix choto
+        for row in c.execute('SELECT %s FROM %s ORDER BY %s ASC' % (self.column, TABLE_NAME, self.column)):
+            value = row[0]
+            if current_bucket == 0 and items_in_current_bucket == 0:
+                self.histogram[current_bucket] = value
+
+            if items_in_current_bucket == items_per_bucket:
+                current_bucket += 1
+                items_in_current_bucket = 0
+                self.histogram[current_bucket] = value
+
+            items_in_current_bucket += 1
+        current_bucket += 1
+        self.histogram[current_bucket] = value
+        print 'end'
 
     def estimate_equal(self, value):
-        pass
+        if value < self.histogram[0]:
+            return 0
+        if value > self.histogram[self.parameter]:
+            return 0
+
+        for bucket in xrange(self.parameter+1):
+            if self.histogram[bucket] > value:
+                #CASE A: between steps
+                return 1.0 / (3 * self.parameter)
+            elif self.histogram[bucket] == value:
+                #CASE B/C: equal to steps
+                if 0 < bucket < self.parameter and self.histogram[bucket+1] != value:
+                    #CASE B1: equal to ONE step and it's NOT FIRST OR LAST
+                    return 1.0 / self.parameter
+                else:
+                    k = 0
+                    while self.histogram[bucket+k+1] == value:
+                        k += 1
+                    if 0 < bucket and self.histogram[self.parameter] != value:
+                        #CASE B2: equal to SEVERAL steps, but NOT FIRST OR LAST
+                        return float(k / self.parameter)
+                    else:
+                        #CASE C: equal to ONE OR SEVERAL STEPS including FIRST OR LAST
+                        return float((k - 0.5) / self.parameter)
+
+    def estimate_lower(self, value):
+        if value < self.histogram[0]:
+            return 0
+        if value > self.histogram[self.parameter]:
+            return self.total
+
+        for bucket in xrange(self.parameter+1):
+            if self.histogram[bucket] > value:
+                #CASE A: between steps
+                return (bucket + 1/3) / self.parameter
+            elif self.histogram[bucket] == value:
+                #CASE B/C: equal to steps
+                if 0 < bucket < self.parameter and self.histogram[bucket+1] != value:
+                    #CASE B1: equal to ONE step and it's NOT FIRST OR LAST
+                    return (bucket - 0.5) / self.parameter
+                else:
+                    k = 0
+                    while self.histogram[bucket+k+1] == value:
+                        k += 1
+                    if 0 < bucket and self.histogram[self.parameter] != value:
+                        #CASE B2: equal to SEVERAL steps, but NOT FIRST OR LAST
+                        return (bucket - 0.5) / self.parameter
+                    else:
+                        #CASE C: equal to ONE OR SEVERAL STEPS including FIRST OR LAST
+                        return 1 - (k - 0.5) / self.parameter
 
     def estimate_greater(self, value):
-        pass
+        return self.total - self.estimate_equal(value) - self.estimate_lower(value)
 
 
 class EstimadorGrupo(Estimator):
-
     def build_struct(self):
         pass
 
