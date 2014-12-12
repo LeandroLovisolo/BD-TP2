919947c11ebcc458ce9f452a76e68098a0a8651c
Leandro Lovisolo <leandro@leandro.me>
2014-12-12 02:54:08 -0300

    Un gráfico por columna

--- source/plot-significance.py
+++ source/plot-significance.py
@@ -5,46 +5,50 @@ import argparse
 import sqlite3
 from plot import Plot
 
-DATABASE = 'significance.sqlite3'
+DATABASE         = 'significance.sqlite3'
+PVALUE_THRESHOLD = 0.05
 
 class SignificancePlot(Plot):
-    def __init__(self, parameters_and_pvalues_per_column, output_path):
-        self.parameters_and_pvalues_per_column = parameters_and_pvalues_per_column
+    def __init__(self, data, output_path):
+        self.data = data
         Plot.__init__(self, output_path)
 
     def do_plot(self, plt):
-        # Asumo que se usan los mismos parámetros en todas las columnas.
-        parameters = None
+        for estimator1, estimator2, comparison, parameters, pvalues in self.data:
+            label = '%s vs %s (%s)' % (estimator1, estimator2, comparison)
+            plt.plot(parameters, pvalues, label=label)
 
-        for column, parameters, pvalues in self.parameters_and_pvalues_per_column:
-            plt.plot(parameters, pvalues, label=column)
+        # Tomo los parametros de la primera comparación, asumiendo que todas
+        # las comparaciones usan los mismos parameters.
+        parameters = self.data[0][3]
 
-            plt.xticks(parameters)
-            plt.xlabel('Par\\\'ametro')
-            plt.ylabel('P-valor')
+        # Threshold
+        plt.hlines(PVALUE_THRESHOLD, parameters[0], parameters[-1],
+                   linestyle='--', color='b', alpha=0.4)
 
-        length = max(parameters) - min(parameters)
-        margin = length * 0.05
-        plt.xlim([min(parameters) - margin, max(parameters) + margin])
+        # Pendiente: hacer funcionar el texto que acompaña a la línea de p-valor.
+        # plt.text(PVALUE_THRESHOLD, parameters[1], 'p = %d' % PVALUE_THRESHOLD,
+        #          verticalalignment='top', horizontalalignment='right')
+
+        plt.xticks(parameters)
+        plt.xlabel('Par\\\'ametro')
+        plt.ylabel('p-valor')
 
         plt.legend()
 
-def plot(estimator1, estimator2, comparison, output_path):
+def plot(column, output_path):
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
 
-    columns = []
-    for row in c.execute('''  SELECT DISTINCT(column)
-                                FROM data
-                               WHERE estimator1 = ?
-                                 AND estimator2 = ?
-                                 AND comparison = ?
-                            ORDER BY column''',
-                         (estimator1, estimator2, comparison)):
-      columns.append(row[0])
+    plots = [('ClassicHistogram',  'DistributionSteps', 'equal'),
+             ('ClassicHistogram',  'EstimadorGrupo',    'equal'),
+             ('DistributionSteps', 'EstimadorGrupo',    'equal'),
+             ('ClassicHistogram',  'DistributionSteps', 'greater'),
+             ('ClassicHistogram',  'EstimadorGrupo',    'greater'),
+             ('DistributionSteps', 'EstimadorGrupo',    'greater')]
 
-    parameters_and_pvalues_per_column = []
-    for column in columns:
+    data = []
+    for estimator1, estimator2, comparison in plots:
       parameters = []
       pvalues = []
       for row in c.execute('''  SELECT parameter1 AS parameter, pvalue
@@ -57,39 +61,20 @@ def plot(estimator1, estimator2, comparison, output_path):
                            (column, estimator1, estimator2, comparison)):
         parameters.append(row[0])
         pvalues.append(row[1])
-      parameters_and_pvalues_per_column.append((column, parameters, pvalues))
+      data.append((estimator1, estimator2, comparison, parameters, pvalues))
 
     conn.close()
 
-    SignificancePlot(parameters_and_pvalues_per_column, output_path)
+    SignificancePlot(data, output_path)
     exit()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generates performance plots')
+    parser = argparse.ArgumentParser(description='Generates significance plots')
 
-
-    parser.add_argument('--hist-vs-diststep-equal',   action='store_true')
-    parser.add_argument('--hist-vs-custom-equal',     action='store_true')
-    parser.add_argument('--diststep-vs-custom-equal', action='store_true')
-
-    parser.add_argument('--hist-vs-diststep-greater',   action='store_true')
-    parser.add_argument('--hist-vs-custom-greater',     action='store_true')
-    parser.add_argument('--diststep-vs-custom-greater', action='store_true')
-
+    parser.add_argument('column', metavar='COL', type=str,
+                        help='which column to plot')
     parser.add_argument('-o', '--output')
 
     args = parser.parse_args()
 
-    if args.hist_vs_diststep_equal:
-      plot('ClassicHistogram', 'DistributionSteps', 'equal', args.output)
-    if args.hist_vs_custom_equal:
-      plot('ClassicHistogram', 'EstimadorGrupo',    'equal', args.output)
-    if args.diststep_vs_custom_equal:
-      plot('DistributionSteps', 'EstimadorGrupo',   'equal', args.output)
-
-    if args.hist_vs_diststep_greater:
-      plot('ClassicHistogram', 'DistributionSteps', 'greater', args.output)
-    if args.hist_vs_custom_greater:
-      plot('ClassicHistogram', 'EstimadorGrupo',    'greater', args.output)
-    if args.diststep_vs_custom_greater:
-      plot('DistributionSteps', 'EstimadorGrupo',   'greater', args.output)
+    plot(args.column, args.output)
