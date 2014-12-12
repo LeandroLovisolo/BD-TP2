2b8e25acd5274982bd4bd91637c57c7d443dbb20
Leandro Lovisolo <leandro@leandro.me>
2014-12-12 00:57:18 -0300

    Usar mismo parámetro para ambos estimadores en cada par. Realizar consultas tanto por equal como por greater.

--- source/significance-tests.py
+++ source/significance-tests.py
@@ -10,8 +10,8 @@ DATABASE    = 'db.sqlite3'
 TABLE       = 'table1'
 NUM_QUERIES = 50
 
-def parameters_for(estimator_class):
-    return [10, 50, 100]
+def parameters_for(estimator1_class, estimator2_class):
+    return [i for i in range(10, 110, 10)]
 
 def queries_for(column):
     conn = sqlite3.connect(DATABASE)
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     c.execute('DROP TABLE IF EXISTS data')
     c.execute('''
               CREATE TABLE data (column     INTEGER,
+                                 comparison TEXT,
                                  estimator1 TEXT,
                                  estimator2 TEXT,
                                  parameter1 INTEGER,
@@ -41,31 +42,33 @@ if __name__ == '__main__':
               ''')
 
     for column in columns:
-        for estimator1_class, estimator2_class in estimator_pairs:
-            queries = queries_for(column)
-            for parameter1 in parameters_for(estimator1_class):
-                for parameter2 in parameters_for(estimator2_class):
-                    e1 = estimator1_class(DATABASE, TABLE, column, parameter1)
-                    e2 = estimator2_class(DATABASE, TABLE, column, parameter2)
+        for comparison in ['equal', 'greater']:
+            for estimator1_class, estimator2_class in estimator_pairs:
+                queries = queries_for(column)
+                for parameter in parameters_for(estimator1_class, estimator2_class):
+                    e1 = estimator1_class(DATABASE, TABLE, column, parameter)
+                    e2 = estimator2_class(DATABASE, TABLE, column, parameter)
 
-                    performances_e1, mean_e1, _ = performance.compute_performance(e1, 'equal', queries)
-                    performances_e2, mean_e2, _ = performance.compute_performance(e2, 'equal', queries)
+                    performances_e1, mean_e1, _ = performance.compute_performance(e1, comparison, queries)
+                    performances_e2, mean_e2, _ = performance.compute_performance(e2, comparison, queries)
 
                     _, p_value = stats.ttest_rel(performances_e1, performances_e2)
 
-                    print 'column: %s\test1: %s\test2: %s\tparam1: %d\tparam2: %d\tmean1: %f\tmean2: %f\tp-value: %f' \
+                    print 'column: %s\tcomparison: %s\test1: %s\test2: %s\tparam1: %d\tparam2: %d\tmean1: %f\tmean2: %f\tp-value: %f' \
                             % (column,
+                               comparison,
                                estimator1_class.__name__,
                                estimator2_class.__name__,
-                               parameter1, parameter2,
+                               parameter, parameter,
                                mean_e1, mean_e2,
                                p_value)
 
-                    c.execute('INSERT INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                    c.execute('INSERT INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
                             (column,
+                             comparison,
                              estimator1_class.__name__,
                              estimator2_class.__name__,
-                             parameter1, parameter2,
+                             parameter, parameter,
                              mean_e1, mean_e2,
                              p_value))
 
